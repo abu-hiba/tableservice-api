0287301a0d4f8f18d6777b226bf70633a181d8b3
Tom Bolton <tbolton96@icloud.com>
2022-05-06 17:22:53 +0100

    Adds ormconfig

---
+++ ormconfig.js
@@ -0,0 +1,14 @@
+module.exports = {
+    type: "postgres",
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT) || 5432,
+    username: process.env.DB_USER,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD,
+    entities: ["dist/**/*.entity{.ts,.js}"],
+    migrations: ["dist/db/migrations"],
+    ssl: {
+        rejectUnauthorized: false
+    },
+    synchronize: process.env.NODE_ENV !== 'production'
+}
